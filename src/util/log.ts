a7bf2185b356be8d398bbba364c2aaaebac8808c
Emin B <zSuckyLP@gmail.com>
2023-11-19 23:31:41 +0100

    custom logging with colored date

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,1 +1,9 @@
+const reset = "\x1b[0m"
+const yellow = (msg: string) => `\x1b[33m${msg}${reset}`;
+
+// overriding log to also print timestamps
+const oldCons = console.log
+console.log = (msg, params) => {
+    oldCons(yellow(`[${new Date().toLocaleString()}]`), ...[msg, ...(params ?? [])])
+}
 export const prefixLog = (prefix: string) => (msg: string) => console.log(`[${prefix}] ${msg}`)
