5e2816f3510054eb59a3e090a19f77aafb063e23
Emin B <zSuckyLP@gmail.com>
2024-04-10 21:13:39 +0200

    show "ALL GUIDES ARE UPDATED TO SEASON x" in /progress

--- src/commands/progress.ts
+++ src/commands/progress.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder, bold, underscore } from "discord.js"
+import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder, bold, italic, underscore } from "discord.js"
 import { allGuides } from "../features/store/guides"
 import { currentSeason, defaultSeason } from "../../.env.json"
 
@@ -10,7 +10,8 @@ export default {
     execute: async (interaction: ChatInputCommandInteraction) => {
         const finished = '✅', wip = '🏗️'
         const guides = await allGuides()
-        await interaction.reply(`${bold("List of accessible/currently worked on guides:")}
+        const fullyUpdated = Object.values(guides).every(guide => guide.season == currentSeason)
+        await interaction.reply(`${bold("List of accessible/currently worked on guides:")}${fullyUpdated ? italic(`\nALL GUIDES ARE UPDATED TO SEASON ${currentSeason}!`) : ''}
 
 ${Object.entries(guides).sort((a, b) => a[1].public != b[1].public ? (b[1].public ? 1 : -1) : a[0].localeCompare(b[0])).map(([champ, guide]) => {
     return `${guide.public ? finished : wip}  ${bold(`${champ}${guide.season != currentSeason ? " " + underscore(`(Season ${guide.season ?? defaultSeason})`) : ''}`)}`
